9328b0d8f9e2fd9a73306c3ea8bc043bfa13ac96
andev-de <github@andev.de>
2025-03-30 11:57:01 +0200

    added Release Notes import

--- David-KnownledgeBase/kb-2-woltlab.php
+++ David-KnownledgeBase/kb-2-woltlab.php
@@ -3,33 +3,13 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set('display_errors', 1);
 
-function create_wiki_page($kb) {
+function create_wiki_page($info, $post_data) {
 	$sessionNumber = 'wsc_583127_user_session';
 	$sessionValue  = '0f2065ffe1a2e49c5b5872bda68a9b9a7748f628c6f7f0f58250aa70e269c971-AXlT19l%2F0JZfnDXFyJr5PsJJMPGi0Q%3D%3D';
 	$xsrfToken = 'd90aaed67151a6eb8e16f7dc868c77313c5390c0923bbb667f8a05ab7ad94211-ZlXsRn634PD%2BuiAgu5gEzA%3D%3D';
 	$cookieValue = $sessionNumber.'='.$sessionValue.'; XSRF-TOKEN='.$xsrfToken;
 	parse_str('t='.$xsrfToken, $tokenArr);
 
-	
-
-	// $article  = "<h1>".$kb['kbid']." - ".$kb['title']."</h1>";
-	$article = "aus Tobit KB importiert - vom ".$kb['date'];
-	if (!empty($kb['link']))
-		$article .= " - <a href='".$kb['link']."'>Beitrag bei Tobit</a>";
-	$article .= "<hr><br>";
-
-	$article .= "<b>Problem</b><hr><p>".$kb['problem']."</p><br><br><br>";
-	$article .= "<b>Antwort</b><hr><p>".$kb['answer']."</p>";
-
-	$post_data['subject'] = $kb['kbid'];
-	$post_data['synonyms'] = '';
-	$post_data['tags'][] = $kb['product'];
-	$post_data['restrictedWriteAccess'] = '';
-	$post_data['category'] = '5';
-	$post_data['excerpt'] = $kb['kbid'].' - '.$kb['title'];
-	$post_data['message'] = $article;
-	// $post_data['messageAttachments_tmpHash'][] = 'c7e0c4c02faa2a0e4b43b01acb0fbf6457822191';
-	// $post_data['isLinkedByParser'] = '1';
 	$post_data['t'] = $tokenArr['t'];
 
 	// print_r($post_data); exit;
@@ -38,7 +18,7 @@ function create_wiki_page($kb) {
 
 	// echo $encodedData; exit;
 
-	echo "generating ",$kb['kbid'],"... ";
+	echo $info;
 
 	// URL to send the POST request to
 	$url = "https://www.david-forum.de/wiki/entry-add/";
@@ -136,6 +116,39 @@ function create_wiki_page($kb) {
 	file_put_contents(dirname(__FILE__).'/response.html', $response);
 }
 
+function create_kb_page($kb) {
+	// $article  = "<h1>".$kb['kbid']." - ".$kb['title']."</h1>";
+	$article = "aus Tobit KB importiert - vom ".$kb['date'];
+	if (!empty($kb['link']))
+		$article .= " - <a href='".$kb['link']."'>Beitrag bei Tobit</a>";
+	$article .= "<hr><br>";
+
+	$article .= "<b>Problem</b><hr><p>".$kb['problem']."</p><br><br><br>";
+	$article .= "<b>Antwort</b><hr><p>".$kb['answer']."</p>";
+
+	$post_data['subject'] = $kb['kbid'];
+	$post_data['synonyms'] = '';
+	$post_data['tags'][] = $kb['product'];
+	$post_data['restrictedWriteAccess'] = '';
+	$post_data['category'] = '5';
+	$post_data['excerpt'] = $kb['kbid'].' - '.$kb['title'];
+	$post_data['message'] = $article;
+
+	create_wiki_page("generating ".$kb['kbid']."... ", $post_data);
+}
+
+function create_rl_page($page) {
+	$post_data['subject'] = 'Version '.$page['version'];
+	$post_data['synonyms'] = '';
+	$post_data['tags'][] = 'Version '.$page['version'];
+	$post_data['restrictedWriteAccess'] = '';
+	$post_data['category'] = '6';
+	$post_data['excerpt'] = 'Release Notes zu Version '.$page['version'].' vom '.$page['date'];
+	$post_data['message'] = $page['notes'];
+
+	create_wiki_page("generating ReleaseNote ".$page['version']."... ", $post_data);
+}
+
 function convert_file_typeA($file) {
 	$kb = array();
 	$fdata = file_get_contents($file);
@@ -211,21 +224,85 @@ function convert_file_typeA($file) {
 	return $kb;
 }
 
-// $files = @glob(dirname(__FILE__).'/kbase/*.html');
-// $files = @glob(dirname(__FILE__).'/kbase/Q-100042.html');
-// $files = @glob(dirname(__FILE__).'/kbase/Q-10003*.html');
-// $files = @glob(dirname(__FILE__).'/kbase/Q-11000*.html');
-$files = @glob(dirname(__FILE__).'/kbase/Q-106893.html');
-// $files = @glob(dirname(__FILE__).'/kbase/Q-11076*.html');
+function read_releasenotes_file($file) {
+	$page = array();
+	$fdata = file_get_contents($file);
 
-$cnt = 0;
-$max = 1;
+	$pfind = '====== Version';
+	$pstart = strpos($fdata, $pfind) + strlen($pfind);
+	$pend = strpos($fdata, ' vom', $pstart - 1);
+	$plen = $pend - $pstart;
+	$page['version'] = trim(substr($fdata, $pstart, $plen));
 
-foreach ($files as $file) {
-	$kb = convert_file_typeA($file);
-	create_wiki_page($kb);
-	$cnt++;
+	$pfind = ' vom ';
+	$pstart = strpos($fdata, $pfind) + strlen($pfind);
+	$pend = strpos($fdata, ' ======', $pstart - 1);
+	$plen = $pend - $pstart;
+	$page['date'] = trim(substr($fdata, $pstart, $plen));
 
-	if ($cnt >= $max)
-		exit;
+	$pfind = 'Release: ';
+	$pstart = strpos($fdata, $pfind) + strlen($pfind);
+	$pend = strpos($fdata, "\n", $pstart - 1);
+	$plen = $pend - $pstart;
+	$page['release'] = trim(substr($fdata, $pstart, $plen));
+
+	$page['notes'] = trim(substr($fdata, 73));
+
+	$page['notes'] = str_replace('\\ ', '<br>', $page['notes']);
+	$page['notes'] = str_replace('  * ', "</li>\n<li>", $page['notes']);
+	// $page['notes'] = preg_replace('/  \* (.+)$/m', "<li>$1</li>", $page['notes']);
+	// $page['notes'] = str_replace("\r</li>", "</li>", $page['notes']);
+	$page['notes'] = str_replace("\r\n\n</li>", "</li>", $page['notes']);
+	$page['notes'] = str_replace("==== Feature ====\n\n</li>", "<b>Feature</b>\n<ul>", $page['notes']);
+	$page['notes'] = str_replace("==== Bugfix ====\n\n</li>", "<b>Bugfix</b><ul>", $page['notes']);
+	$page['notes'] = str_replace("=====  ", "</ul>\n<br><br>\n<b>", $page['notes']);
+	$page['notes'] = str_replace(" =====", "</b>\n<hr>", $page['notes']);
+	$page['notes'] = str_replace("\n\n\n</ul>", "</ul>\n\n", $page['notes']);
+	$page['notes'] = substr($page['notes'], 15);
+	$page['notes'] = str_replace("\n</ul>\n<br><br>", "</ul>\n\n<br><br>", $page['notes']);
+	$page['notes'] .= "</li>\n</ul>";
+
+	file_put_contents(dirname(__FILE__).'/notes.txt', $page['notes']);
+
+	// echo print_r($page, true),"\n";
+	return $page;
+}
+
+// Import KB
+if (1 == 0) {
+	// $files = @glob(dirname(__FILE__).'/kbase/*.html');
+	$files = @glob(dirname(__FILE__).'/kbase/Q-10003*.html');
+	// $files = @glob(dirname(__FILE__).'/kbase/Q-10003*.html');
+	// $files = @glob(dirname(__FILE__).'/kbase/Q-11000*.html');
+	// $files = @glob(dirname(__FILE__).'/kbase/Q-106893.html');
+	// $files = @glob(dirname(__FILE__).'/kbase/Q-11076*.html');
+	
+	$cnt = 0;
+	$max = 10;
+	
+	foreach ($files as $file) {
+		$kb = convert_file_typeA($file);
+		create_kb_page($kb);
+		$cnt++;
+	
+		if ($cnt >= $max)
+			exit;
+	}
+}
+
+// Import ReleaseNotes
+if (1 == 1) {
+	$files = @glob(dirname(__FILE__).'/releasenotes/*.txt');
+
+	$cnt = 0;
+	$max = 140;
+
+	foreach ($files as $file) {
+		$item = read_releasenotes_file($file);
+		create_rl_page($item);
+		$cnt++;
+	
+		if ($cnt >= $max)
+			exit;
+	}
 }
